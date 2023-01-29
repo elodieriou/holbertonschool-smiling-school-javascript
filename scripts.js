d8bf813aa55a02ff95ce2f764ad87cfabc5f5ca6
Elodie Riou <elodie.riou@hotmail.com>
2023-01-29 16:21:29 +0100

    FEATURE: implement dropdown filters and lists of courses associated

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const urlQuotes = "https://smileschool-api.hbtn.info/quotes";
 const urlTutorials = "https://smileschool-api.hbtn.info/popular-tutorials";
 const urlLatest = "https://smileschool-api.hbtn.info/latest-videos";
+const urlCourses = "https://smileschool-api.hbtn.info/courses";
 
 function getSectionQuotes() {
     $('.loader').show();
@@ -54,7 +55,7 @@ function getSectionTutorials() {
         type: "GET",
         url: urlTutorials,
         success: function (data) {
-            retrieveData(this.url, data);
+            retrieveDataForCarousel(this.url, data);
         },
         error: function () {
             alert("Server Error");
@@ -72,7 +73,7 @@ function getSectionLatest() {
         type: "GET",
         url: urlLatest,
         success: function (data) {
-            retrieveData(this.url, data);
+            retrieveDataForCarousel(this.url, data);
         },
         error: function () {
             alert("Server Error");
@@ -84,7 +85,7 @@ function getSectionLatest() {
     });
 }
 
-function retrieveData(url, data) {
+function retrieveDataForCarousel(url, data) {
     for (let i = 0; i < data.length; i++) {
         buildVideos(url, i, data[i].thumb_url, data[i].title, data[i]["sub-title"], data[i].author_pic_url, data[i].author, data[i].star, data[i].duration)
     }
@@ -150,9 +151,141 @@ function calculateStars(number_stars) {
     return html
 }
 
+function getSectionFilters() {
+
+    $.ajax({
+        type: "GET",
+        url: urlCourses,
+        success: function (data) {
+            buildDefaultValue(data.topic, data["sort"], data.q);
+            buildDropdown(data.topics, data.sorts);
+        },
+        error: function () {
+            alert("Server Error");
+            console.log("");
+        }
+    });
+}
+
+function buildDefaultValue(topic, sort, q) {
+    $('#topic-dropdown button').append(
+        $('<span></span>').attr("data-value", `${topic}`).text(topic.replace(/^./, topic[0].toUpperCase()))
+    );
+    $('#sort-dropdown button').append(
+        $('<span></span>').attr("data-value", `${sort}`).text(sort.replace(/^./, sort[0].toUpperCase()).replace(/_/, " "))
+    );
+    $('#search-bar').val(q);
+
+    getSectionCourses();
+}
+
+function buildDropdown(topics, sorts) {
+    for (let i = 0; i < topics.length; i++) {
+        const displayValue = topics[i].replace(/^./, topics[i][0].toUpperCase());
+        $('#topic-menu').append(
+            $('<a></a>').attr({class: "dropdown-item", href: "#", ["data-value"]: topics[i]}).text(displayValue).click(function (event) {
+                event.preventDefault();
+                $('#topic-dropdown button span').attr("data-value", $(this).attr("data-value")).text(this.text);
+                getSectionCourses();
+            })
+        );
+    }
+
+    for (let i = 0; i < sorts.length; i++) {
+        const displayValue = sorts[i].replace(/^./, sorts[i][0].toUpperCase()).replace(/_/, " ");
+        $('#sort-menu').append(
+            $('<a></a>').attr({class: "dropdown-item", href: "#", ["data-value"]: sorts[i]}).text(displayValue).click(function (event) {
+                event.preventDefault();
+                $('#sort-dropdown button span').attr("data-value", $(this).attr("data-value")).text(this.text);
+                getSectionCourses();
+            })
+        );
+    }
+}
+
+function getSectionCourses() {
+    $('.loader').show();
+    const data = {
+        q: $('#search-bar').val(),
+        topic: $('#topic-dropdown button span').attr("data-value"),
+        sort: $('#sort-dropdown button span').attr("data-value")
+    }
+    const url = `${urlCourses}?q=${data.q}&topic=${data.topic}&sort=${data.sort}`;
+    $.ajax({
+        type: "GET",
+        url: url,
+        data: data,
+        success: function (data) {
+            $('#js-content-courses').empty();
+            calculateNumberCourses(data.courses);
+            retrieveDataForCourses(data.courses);
+        },
+        error: function () {
+            alert("Server Error");
+            console.log("");
+        },
+        complete: function () {
+            $('.loader').hide();
+        }
+    });
+}
+
+function retrieveDataForCourses(data) {
+    for (let i = 0; i < data.length; i++) {
+        buildCard(data[i].thumb_url, data[i].title, data[i]["sub-title"], data[i].author_pic_url, data[i].author, data[i].star, data[i].duration);
+    }
+}
+
+function calculateNumberCourses(data) {
+    const number = data.length;
+    const displayValue = number === 1 ? `${number} video` : `${number} videos`;
+    $('#js-number-videos').text(`${displayValue}`);
+}
+
+function buildCard(thumb_image, title, sub_title, author_image, author_name, stars, duration) {
+
+    const html_stars = calculateStars(stars);
+
+    $('#js-content-courses').append(
+        $('<div></div>').attr({class: "card border-0 col-12 col-sm-6 col-md-4 col-lg-3 mt-5", style: "width: 18rem"}).append(
+            $('<img>').attr({
+                class: "card-img-top img-fluid",
+                src: thumb_image,
+                alt: "tuto"
+            }),
+            $('<img>').attr({
+                class: "position-absolute position-play",
+                src: "images/play.png",
+                alt: "play",
+                width: "64px",
+                height: "64px"
+            }),
+            $('<div></div>').attr({class: "card-body d-flex flex-column"}).append(
+                $('<h3></h3>').attr({class: "card-title font-weight-bold font-size-large"}).text(title),
+                $('<p></p>').attr({class: "card-text"}).text(sub_title),
+                $('<div></div>').attr({class: "d-flex"}).append(
+                    $('<img>').attr({
+                        class: "rounded-circle mr-3",
+                        src: author_image,
+                        alt: "profil",
+                        width: "30px",
+                        height: "30px"
+                    }),
+                    $('<strong></strong>').text(author_name)
+                ),
+                $('<div></div>').attr({class: "d-flex justify-content-between mt-3"}).append(
+                    $('<div></div>').append(html_stars),
+                    $('<strong></strong>').text(duration)
+                )
+            )
+        )
+    );
+}
 
 $(function () {
     getSectionQuotes();
     getSectionTutorials();
     getSectionLatest();
+    getSectionFilters();
+    getSectionCourses();
 })
